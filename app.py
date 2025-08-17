fdde8bff3573ffac706baa3657de02c1f75abc78
Vaibhavee Singh <vaibhaveesingh89@gmail.com>
2025-08-17 17:21:09 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,21 @@
+from openai import OpenAI
+
+client = OpenAI(
+  base_url = "https://integrate.api.nvidia.com/v1",
+  api_key = "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"
+)
+
+completion = client.chat.completions.create(
+  model="openai/gpt-oss-120b",
+  messages=[{"role":"user","content":"Provide me an article on Machine Learning"}],
+  temperature=1,
+  top_p=1,
+  max_tokens=4096,
+  stream=True
+)
+
+
+for chunk in completion:
+  if chunk.choices[0].delta.content is not None:
+    print(chunk.choices[0].delta.content, end="")
+
